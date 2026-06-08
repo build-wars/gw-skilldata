dafe9483600c816f1bd0293f62f4745ba67285f1
smiley <codemasher@users.noreply.github.com>
2026-06-08 17:30:16 +0200

    :octocat: combined skilldata builder update

--- tools/build-json.php
+++ tools/build-json.php
@@ -16,6 +16,7 @@ namespace Buildwars\GWSkillDataTools;
 use Buildwars\GWSkillData\SkillDataInterface;
 use chillerlan\Utilities\File;
 use chillerlan\Utilities\Str;
+use function sprintf;
 use function str_replace;
 
 /**
@@ -23,30 +24,44 @@ use function str_replace;
  */
 require_once __DIR__.'/common.php';
 
+const combinedJSON = __DIR__.'/../data/json-full/skilldata-combined.json';
+
 $jsonData = File::loadJSON(dataFile, true);
+$jsonLang = [];
+
+foreach(langFiles as [$abbr, $file]){
+	$jsonLang[$abbr] = File::loadJSON($file, true);
+}
 
 foreach($jsonData['skilldata'] as $skillID => &$skillData){
-	foreach(langFiles as [$abbr, $file]){
-		$jsonLang = File::loadJSON($file, true);
+	foreach(['de', 'en'] as $abbr){
+		$lang = $jsonLang[$abbr]['skilldesc'][$skillID];
 
-		unset($jsonLang['skilldesc'][$skillID]['id']);
-
-		$skillData['lang'][$abbr]                    = $jsonLang['skilldesc'][$skillID];
-		$skillData['lang'][$abbr]['campaign']        = SkillDataInterface::CAMPAIGNS[$skillData['campaign']]['name'][$abbr];
-		$skillData['lang'][$abbr]['profession']      = SkillDataInterface::PROFESSIONS[$skillData['profession']]['name'][$abbr];
-		$skillData['lang'][$abbr]['profession_abbr'] = SkillDataInterface::PROFESSIONS[$skillData['profession']]['abbr'][$abbr];
-		$skillData['lang'][$abbr]['attribute']       = SkillDataInterface::ATTRIBUTES[$skillData['attribute']]['name'][$abbr];
-		$skillData['lang'][$abbr]['type']            = SkillDataInterface::SKILLTYPES[$skillData['type']]['name'][$abbr];
-
-		$skill = [
-			'$schema' => 'https://raw.githubusercontent.com/build-wars/gw-skilldata/refs/heads/main/data/json-skills/skill.schema.json',
-			'skill'   => $skillData,
+		$skillData['lang'][$abbr] = [
+			'name'            => $lang['name'],
+			'description'     => $lang['description'],
+			'concise'         => $lang['concise'],
+			'campaign'        => SkillDataInterface::CAMPAIGNS[$skillData['campaign']]['name'][$abbr],
+			'profession'      => SkillDataInterface::PROFESSIONS[$skillData['profession']]['name'][$abbr],
+			'profession_abbr' => SkillDataInterface::PROFESSIONS[$skillData['profession']]['abbr'][$abbr],
+			'attribute'       => SkillDataInterface::ATTRIBUTES[$skillData['attribute']]['name'][$abbr],
+			'type'            => SkillDataInterface::SKILLTYPES[$skillData['type']]['name'][$abbr],
 		];
 
-		File::save(__DIR__.'/../data/json-skills/'.$skillID.'.json', str_replace('    ', "\t", Str::jsonEncode($skill)));
 	}
+
+	$skill = [
+		'$schema' => 'https://raw.githubusercontent.com/build-wars/gw-skilldata/refs/heads/main/data/json-skills/skill.schema.json',
+		'skill'   => $skillData,
+	];
+
+	$logger->info(sprintf('JSON for [%-4s] %s ', $skillID, $skillData['lang']['en']['name']));
+
+	File::save(__DIR__.'/../data/json-skills/'.$skillID.'.json', str_replace('    ', "\t", Str::jsonEncode($skill)));
 }
+
+$logger->info(sprintf('JSON for combined skilldata: %s ', File::realpath(combinedJSON)));
 
 $jsonData['$schema'] = 'https://raw.githubusercontent.com/build-wars/gw-skilldata/refs/heads/main/data/json-full/skilldata-combined.schema.json';
 
-File::save(__DIR__.'/../data/json-full/skilldata-combined.json', str_replace('    ', "\t", Str::jsonEncode($jsonData)));
+File::save(combinedJSON, str_replace('    ', "\t", Str::jsonEncode($jsonData)));
